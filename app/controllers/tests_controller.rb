11cd296b390d4f8062bcd08c4f20d34ba74a734e
Casey McGuire <caseymcguire90@gmail.com>
2015-03-10 23:42:47 -0700

    Added some comments and plans.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,16 +1,22 @@
 class TestsController < ApplicationController
 
-  
+  #need to authenticate admin
   
   def index
     @test = Test.all
   end
 
   def new
+    #if there are no parameters, we don't know which problem and
+    #which language we're creating so redirect
+    if params == nil || params[:problem_id] == nil || params[:language_id] == nil
+      redirect_to problems_path
+    end
     
   end
   
   def create
+    #need to ensure a record with same problem_id and language_id doesn't already exist
   end
 
   def edit
